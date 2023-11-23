073e454b476c52eac2df617e5464e86c9cc0a4b4
AkanTahu <hhamzalnur10@gmail.com>
2023-11-23 16:16:17 +0700

    Initial commit

---
+++ FIXSPKmaybe.py
@@ -0,0 +1,55 @@
+from flask import Flask  , render_template,request,make_response,jsonify,url_for,redirect,Response
+import numpy as np
+import pandas as pd
+import operator
+import requests
+from pyDecision.algorithm import edas_method
+import json
+from flask_cors import CORS
+
+# Connect to the MySQL database
+
+
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://127.0.0.1:8000"}}) 
+
+@app.route("/test" , methods=['GET'])
+def testing():
+    return "hello"
+
+@app.route("/post-rank" , methods=['POST'])
+def postRank():
+    data = request.json['api_datas']
+    print(data)
+    
+    df = pd.DataFrame(data)
+    namamhs = df["nama"]
+    idmhs = df["id"]
+
+    columns_of_interest = ['ipk', 'alpha', 'sertifikat']
+    dataset = df[columns_of_interest].values
+    
+    criterion_type = ['max', 'min', 'max']
+
+    weights = [0.25, 0.40, 0.20]
+
+    rank = edas_method(dataset, criterion_type, weights,graph = False, verbose = False)
+
+    hasil = []
+    
+    for i in range(0, rank.shape[0]):
+        hasil.append({
+            "mhsId":int(idmhs[i]),
+            "mhsname":namamhs[i],
+            "alternative":'a' + str(i+1),
+            "spk":round(rank[i], 4)
+        })
+
+    sorted_list = sorted(hasil, key=lambda x: x['spk'], reverse=True)
+
+    send_data = json.dumps(sorted_list)
+    
+    return send_data
+        
+    
